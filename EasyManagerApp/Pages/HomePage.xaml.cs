41dd813b81587e5dbc96177d36f34f685f02a681
PedroCarvalhoF <operacoes.pedro@gmail.com>
2025-05-12 04:52:33 -0300

    Contruindo Home Page v1.3

--- EasyManagerApp/Pages/HomePage.xaml.cs
+++ EasyManagerApp/Pages/HomePage.xaml.cs
@@ -14,7 +14,7 @@ public partial class HomePage : ContentPage
         base.OnAppearing();
 
         lblBoasVindas.Text = $"{ConfiguracoesGlobalApp.GetUsuarioLogadoResponse().Nome} , Bem Vindo";
-        lblFilialSelecionada.Text = $"Filial Selecionada: {ConfiguracoesGlobalApp.GetFilialSelecionada().NomeFilial}";
+        lblFilialSelecionada.Text = $"Filial: {ConfiguracoesGlobalApp.GetFilialSelecionada().NomeFilial}";
 
     }
     private async void Filial_Tapped(object sender, TappedEventArgs e)
